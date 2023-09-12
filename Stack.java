965c0c05d7d3bc6576b70e815776fa3c0c07d5aa
mael098 <juanvillegasmorales@outlook.com>
2023-09-11 19:03:57 -0600

    agrege  el metodo pop y el mostrar

---
+++ Stack.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class Stack {
+    private int size;
+    private int top;
+    private int[] stack;
+
+    public Stack(int size) {
+        this.size = size;
+        this.top = -1;
+        this.stack = new int[size];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
+    }
+
+    public void push(int element) {
+        if (!isFull()) {
+            top++;
+            stack[top] = element;
+        } else {
+            System.out.println("Pila llena");
+        }
+    }
+
+    public int pop() {
+        if (!isEmpty()) {
+            int poppedElement = stack[top];
+            top--;
+            return poppedElement;
+        } else {
+            System.out.println("Pila vacía, no se puede hacer pop.");
+            return -1; // Valor de retorno de error
+        }
+    }
+
+    public void mostrar() {
+        if (!isEmpty()) {
+            System.out.println("Elementos en la pila:");
+            for (int i = 0; i <= top; i++) {
+                System.out.println(stack[i]);
+            }
+        } else {
+            System.out.println("La pila está vacía.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack pila = new Stack(5);
+
+        pila.push(1);
+        pila.push(2);
+        pila.push(3);
+        pila.push(4);
+        pila.push(5);
+        pila.push(6);
+
+        System.out.println("La pila está vacía: " + pila.isEmpty());
+
+        System.out.println("Elemento pop: " + pila.pop());
+        System.out.println("Elemento pop: " + pila.pop());
+
+        pila.mostrar();
+    }
+}
+// jdoodte.com/online-java-copilet/
--- index.js
+++ index.js
@@ -1,33 +1,61 @@
-
-
-class Stask {
-  constructor(sezi) {
-    this.sezi = sezi
-    this.top = -1
-    this.pila = new Array(sezi)
-  }
-  emply() {
-    return this.top === -1
+class Stack {
+  constructor(size) {
+    this.size = size;
+    this.top = -1;
+    this.stack = new Array(size);
   }
 
-  push(e) {
-    if (this.top < this.sezi - 1) {
-        this.top++
-        this.pila[this.top] = e
+  isEmpty() {
+    return this.top === -1;
+  }
+
+  isFull() {
+    return this.top === this.size - 1;
+  }
+
+  push(element) {
+    if (!this.isFull()) {
+      this.top++;
+      this.stack[this.top] = element;
+    } else {
+      console.log("Pila llena");
     }
-    else{
-      console.log("pila llena");
-       
+  }
+
+  pop() {
+    if (!this.isEmpty()) {
+      const poppedElement = this.stack[this.top];
+      this.top--;
+      return poppedElement;
+    } else {
+      console.log("Pila vacía, no se puede hacer pop.");
+    }
+  }
+
+  mostrar() {
+    if (!this.isEmpty()) {
+      console.log("Elementos en la pila:");
+      for (let i = 0; i <= this.top; i++) {
+        console.log(this.stack[i]);
+      }
+    } else {
+      console.log("La pila está vacía.");
     }
   }
 }
-const pila = new Stask(5)
 
-pila.push(1)
-pila.push(2)
-pila.push(3)
-pila.push(4)
-pila.push(5)
-pila.push(6)
+const pila = new Stack(5);
 
-console.log("la pila esta vacia", pila.emply());
+pila.push(1);
+pila.push(2);
+pila.push(3);
+pila.push(4);
+pila.push(5);
+pila.push(6);
+
+console.log("La pila está vacía:", pila.isEmpty());
+
+console.log("Elemento pop:", pila.pop());
+console.log("Elemento pop:", pila.pop());
+
+pila.mostrar();
